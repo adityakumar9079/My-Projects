e117a0b0ac8bdcc7d2eac3ff779160102a3d402a
Aditya <ar320501@gmail.com>
2026-05-01 19:05:53 +0530

    Updated Financial project

--- Financial_project.py
+++ Financial_project.py
@@ -1,1 +1,8 @@
 print("Hello, welcome to the financial project!")
+Amount = float(input("Please enter the amount you want to invest: "))
+Rate = float(input("Please enter the annual interest rate (in percentage): "))
+Years = int(input("Please enter the number of years you want to invest: "))
+# Calculate the future value of the investment
+Future_Value = Amount * (1 + Rate / 100) ** Years
+print(f"The future value of your investment will be: {Future_Value:.2f}")
+          Adhejsejfiorioh
